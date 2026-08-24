87554a8302d03c21671e480183e983731267e3f5
Enoshjaguar <dasarienosh013@gmail.com>
2026-08-24 18:03:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
-	let res = []
-	let cur_chunk = []
-	let cur_sum = 0
+    let res = [];
+    let cur_chunk = [];
+    let cur_sum = 0;
 
-	for(let i=0;i<arr.length;i++){
-		if(cur_sum+arr[i]>n){
-			res.push(cur_chunk)
-			cur_chunk=[arr[i]]
-			cur_sum=arr[i]
-		}
-		else{
-			cur_chunk.push(arr[i])
-			cur_sum=cur_sum+arr[i]
-			
-		}
-	}
+    for (let i = 0; i < arr.length; i++) {
 
-	if(cur_chunk.length>0){
-		res.push(cur_chunk)
-	}
-	return res
-	
-	
+        if (cur_sum + arr[i] > n) {
+            res.push(cur_chunk);
+
+            cur_chunk = [arr[i]];
+            cur_sum = arr[i];
+        } 
+        else {
+            cur_chunk.push(arr[i]);
+            cur_sum = cur_sum + arr[i];
+        }
+    }
+
+    if (cur_chunk.length > 0) {
+        res.push(cur_chunk);
+    }
+
+    return res;
 };
 
 const n = Number(prompt("Enter n: "));
-console.log(divide,arr,n)
 
-
-
+console.log(divide(arr, n));
